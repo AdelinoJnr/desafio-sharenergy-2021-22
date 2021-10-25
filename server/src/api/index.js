1a8a5e4c8bb8edf5cb0ae59003cc968a51e0afc6
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-25 13:04:10 -0300

    feat: rota CRUD para users

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,9 +2,14 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 
+const usersRoute = require('../routes/users');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Users
+app.use('/api/users', usersRoute);
 
 module.exports = app;
---
+++ server/src/routes/users.js
@@ -0,0 +1,10 @@
+const User = require('../controllers/users');
+const router = require('express').Router();
+
+router.post('/', User.create);
+router.get('/', User.getAll);
+router.get('/:id', User.getById);
+router.put('/:id', User.update);
+router.delete('/:id', User.remove);
+
+module.exports = router;
