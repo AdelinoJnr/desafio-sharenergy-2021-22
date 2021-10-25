475e113c1cfcc45dc0fd3373a3f457b72dd3c43c
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-25 12:29:27 -0300

    feat: connection mongodb

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,5 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
