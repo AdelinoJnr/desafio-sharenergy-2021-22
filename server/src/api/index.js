9cd847fa928b3c856a684ca1bdc6d744750f8749
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-26 12:48:40 -0300

    feat: rota GET para usinas

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,13 +3,17 @@ const express = require('express');
 const cors = require('cors');
 
 const usersRoute = require('../routes/users');
+const factorysRouter = require('../routes/factorys');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Users
+// Usuários
 app.use('/api/users', usersRoute);
+
+// Usinas
+app.use('/api/factorys', factorysRouter);
 
 module.exports = app;
