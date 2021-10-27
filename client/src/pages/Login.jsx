f73f1801a452dbc700a81f0daf5b0e3a65bee7b5
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-27 10:19:09 -0300

    style: estilizacao da pagina login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,23 +2,32 @@ import React, { useState, useContext } from 'react';
 import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
 
+import FormLogin from '../components/FormLogin';
 import { UserContext } from '../context/userContext';
 
 import { userLogin } from '../services/api';
 
+import './styles.css';
+
 function Login() {
   const { setToken, token } = useContext(UserContext);
 
-  const [email, setEMail] = useState('');
-  const [password, setPassword] = useState('');
   const [error, setError] = useState();
   const checked = token ? true : false;
 
 
-  const handleClick = async (ev) => {
+  const handleClick = async (ev, email, password) => {
     ev.preventDefault();
     const payload = { email, password };
     await userLogin(payload, setError, setToken);
+  };
+
+  const renderError = () => {
+    return (
+      <div className="content-error">
+        <p>{error}</p>
+      </div>
+    );
   };
 
   if (checked) {
@@ -27,26 +36,14 @@ function Login() {
 
   return (
     <>
-      <form>
-        <h2>Login</h2>
-        <input
-          onChange={(ev) => setEMail(ev.target.value)}
-          value={email}
-          type="text"
-          placeholder="E-mail"
-          required
-        />
-        <input
-          onChange={(ev) => setPassword(ev.target.value)}
-          value={password}
-          type="password"
-          placeholder="Senha"
-          required
-        />
-        <button onClick={handleClick} type="submit">Entrar</button>
-        { error && <p>{error}</p> }
-        <Link to="/register">Cadastrar</Link>
-      </form>
+      <main className="main-login">
+        <h2 className="title-pages">Login</h2>
+        <FormLogin handleClick={handleClick} />
+        { error && renderError() }
+        <div className="content-link-register">
+          <span>Ainda nao possui cadastro? <Link to="/register">Cadastre-se</Link> </span>
+        </div>
+      </main>
     </>
   );
 }
